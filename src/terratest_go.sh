85c7f4335be518ce2e80402fe12402d95ded27cd
Sohan <31764946+sohanyadav@users.noreply.github.com>
2019-12-27 10:51:27 +0530

    Update terratest_go.sh

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -3,15 +3,26 @@
 function goTest {
   
   echo "install the terratest dependencies"
-  which go
+  apk add -d --update --no-cache go gcc build-base
+  echo "Install Go for terratest"
+  if [ -x /github/home/.go ]; then
+    echo exists
+else
+  apk add --update --no-cache go gcc build-base
   echo "Install Go for terratest"
   wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.13.2
-   apk add -d --update --no-cache go gcc build-base
-
-  which go
   echo "Install Go package fo terratest"
-  go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
+  go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/asser
+fi
+
+#   wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
+#  | bash -s -- --version 1.13.2
+#    apk add -d --update --no-cache go gcc build-base
+
+#   which go
+#   echo "Install Go package fo terratest"
+#   go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
 
   # Gather the output of `teratest`.
   echo "teratest: info: teratest run configuration  in ${tfWorkingDir}"
